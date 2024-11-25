4fa0a5191f3995f2885fb16fd4af89efb36c984a
seghiaranass <anass.segh97@gmail.com>
2024-11-25 11:10:18 +0100

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Product Info Page',
+    'version': '16.0.1.0.0',
+    'depends': ['website', 'product'],
+    'data': [
+        'views/templates.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ controllers/controllers.py
@@ -0,0 +1,14 @@
+from odoo import http
+from odoo.http import request
+
+class ProductInfoController(http.Controller):
+
+    @http.route('/product/<int:product_id>', type='http', auth='public', website=True)
+    def product_page(self, product_id):
+        product = request.env['product.template'].sudo().browse(product_id)
+        if not product.exists():
+            return request.render('website.404')
+        
+        return request.render('product_info_page.product_page_template', {
+            'product': product
+        })
